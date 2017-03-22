9f224e176531994f578c75192c64249aee8f6ce7
lenoch <lenoch@medien.rwth-aachen.de>
2017-03-22 13:43:42 +0100

    Fix scan and valuer error with NullTime.

--- nulltime.go
+++ nulltime.go
@@ -5,6 +5,9 @@ import (
   "encoding/json"
   "bytes"
   "time"
+  "database/sql/driver"
+  "github.com/juju/errors"
+  "fmt"
 )
 
 type NullTime mysql.NullTime
@@ -39,4 +42,29 @@ func (nt *NullTime) UnmarshalJSON(b []byte) error {
   }
 
   return nil
+}
+
+
+// Scan implements the Scanner interface.
+// The value type must be time.Time or string / []byte (formatted time-string),
+// otherwise Scan fails.
+func (nt *NullTime) Scan(value interface{}) (err error) {
+
+  // TODO Fix me - for some reason nested methods are not found.
+  // This should work for now...
+  f := mysql.NullTime{}
+  if err := f.Scan(value); err != nil {
+    return errors.New(fmt.Sprintf("Unable to parse time value: %s", err.Error()))
+  }
+
+  nt.Valid, nt.Time = f.Valid, f.Time
+  return nil
+}
+
+// Value implements the driver Valuer interface.
+func (nt NullTime) Value() (driver.Value, error) {
+  if !nt.Valid {
+    return nil, nil
+  }
+  return nt.Time, nil
 }
