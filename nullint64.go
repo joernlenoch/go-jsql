4388c46ae84e74fa62b2ada11844a9de32c855cc
lenoch <lenoch@medien.rwth-aachen.de>
2018-02-23 11:04:52 +0100

    Remove wrong kataras dependency. Remove superfluous sql parsing methods from NullInt64.

--- nullint64.go
+++ nullint64.go
@@ -1,23 +1,20 @@
 package jsql
 
 import (
-	"bytes"
-	"database/sql"
-	"database/sql/driver"
-	"encoding/json"
-	"fmt"
-	"strconv"
-
-	"github.com/kataras/go-errors"
+  "bytes"
+  "database/sql"
+  "encoding/json"
+  "fmt"
+  "strconv"
 )
 
 type NullInt64 struct {
-	sql.NullInt64
+  sql.NullInt64
 }
 
 func NewNullInt64(s interface{}) NullInt64 {
-	n, _ := TryNullInt64(s)
-	return n
+  n, _ := TryNullInt64(s)
+  return n
 }
 
 // Create a new NullFloat.
@@ -90,61 +87,26 @@ func (nt NullInt64) ToValue() interface{} {
 
 func (nt NullInt64) MarshalJSON() ([]byte, error) {
 
-	if !nt.Valid {
-		return []byte("null"), nil
-	}
+  if !nt.Valid {
+    return []byte("null"), nil
+  }
 
-	return json.Marshal(nt.Int64)
+  return json.Marshal(nt.Int64)
 }
 
 func (nt *NullInt64) UnmarshalJSON(b []byte) error {
-	nt.Valid = false
+  nt.Valid = false
 
-	if bytes.Equal(b, []byte("null")) {
-		return nil
-	}
+  if bytes.Equal(b, []byte("null")) {
+    return nil
+  }
 
-	if len(b) >= 0 {
-		if err := json.Unmarshal(b, &nt.Int64); err != nil {
-			return err
-		}
-		nt.Valid = true
-	}
+  if len(b) >= 0 {
+    if err := json.Unmarshal(b, &nt.Int64); err != nil {
+      return err
+    }
+    nt.Valid = true
+  }
 
-	return nil
-}
-
-func (nt *NullInt64) Scan(value interface{}) error {
-	nt.Valid = false
-
-	if value == nil {
-		return nil
-	}
-
-	var ok bool
-	nt.Int64, ok = value.(int64)
-	if !ok {
-
-		str, ok := value.(string)
-		if !ok {
-			return errors.New(fmt.Sprintf("Unable to parse value '%s'", value))
-		}
-
-		var err error
-		nt.Int64, err = strconv.ParseInt(str, 10, 64)
-		if err != nil {
-			return err
-		}
-	}
-
-	nt.Valid = true
-	return nil
-}
-
-func (nt NullInt64) Value() (driver.Value, error) {
-
-	if !nt.Valid {
-		return nil, nil
-	}
-	return nt.Int64, nil
+  return nil
 }
